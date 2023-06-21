eb048f9e305435e7ca53e1963dc0b576d9865b7e
Toke Friis <tokefriis@gmail.com>
2023-06-21 21:36:45 +0200

    removed FAB

--- lib/main.dart
+++ lib/main.dart
@@ -51,14 +51,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -115,20 +107,10 @@ class _MyHomePageState extends State<MyHomePage> {
               const Text(
                 'You have pushed the button this many times:',
               ),
-              Text("he"),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
             ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
