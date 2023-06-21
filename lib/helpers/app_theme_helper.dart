6e80599722a0b0abd72c2d09f2aa026fccbeafd1
Toke Friis <tokefriis@gmail.com>
2023-06-21 21:35:42 +0200

    made Theme textstyle for headers, body text and subtitles

---
+++ lib/helpers/app_theme_helper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+TextTheme getAppTextTheme(context) {
+  return Theme.of(context).textTheme;
+}
+
+ColorScheme getAppColorScheme(context) {
+  return Theme.of(context).colorScheme;
+}
