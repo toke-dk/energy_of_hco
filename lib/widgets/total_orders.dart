cbd267f66029a7c94066d0fb08ef9f1a921825b7
Toke Friis <tokefriis@gmail.com>
2023-06-22 14:12:29 +0200

    changed height of total orders listview

--- lib/widgets/total_orders.dart
+++ lib/widgets/total_orders.dart
@@ -7,25 +7,27 @@ class TotalOrders extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 230,
       child: ListView.separated(
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
-            return Column(
-              children: [
-                Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 14, horizontal: 10),
-                    child: ShowProductDetails(
-                      favouriteIcon: true,
-                      title: "John D.",
-                      subTitle: "3 items (2 x monster, 1x red bull)",
-                      productPrice: "50,32 kr.",
-                      optionIcon: Icons.edit,
-                      productImage: Image.network(
-                          'https://cdn.bevco.dk/cdn-cgi/image/format%3Dauto/https%3A//cdn.bevco.dk/thumbnail/d3/15/55/1644401459/monster-energy-24x50-cl-daase-449cc_644x644.png'),
-                    )),
-              ],
+            return Center(
+              child: Column(
+                children: [
+                  Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 14, horizontal: 10),
+                      child: ShowProductDetails(
+                        favouriteIcon: true,
+                        title: "John D.",
+                        subTitle: "3 items (2 x monster, 1x red bull)",
+                        productPrice: "50,32 kr.",
+                        optionIcon: Icons.edit,
+                        productImage: Image.network(
+                            'https://cdn.bevco.dk/cdn-cgi/image/format%3Dauto/https%3A//cdn.bevco.dk/thumbnail/d3/15/55/1644401459/monster-energy-24x50-cl-daase-449cc_644x644.png'),
+                      )),
+                ],
+              ),
             );
           },
           separatorBuilder: (context, idex) => const SizedBox(
