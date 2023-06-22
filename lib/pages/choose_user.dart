a1dc02604d41b1e60bd14326f4b0eba8ed4b3f5a
Toke Friis <tokefriis@gmail.com>
2023-06-22 20:08:56 +0200

    made a form to become a member

--- lib/pages/choose_user.dart
+++ lib/pages/choose_user.dart
@@ -16,37 +16,192 @@ class ChooseUser extends StatelessWidget {
         body: Container(
             alignment: Alignment.topCenter,
             padding: const EdgeInsets.all(15),
-            child: MyPaper(
-              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              width: MediaQuery.of(context).size.width * 1,
-              child: DataTable(
-                  showCheckboxColumn: false,
-                  columns: [
-                    DataColumn(
-                        label: Expanded(
-                      child: Text(
-                        "Name",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: getAppColorScheme(context).primary),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                MyPaper(
+                  padding: const EdgeInsets.all(10),
+                  borderRadius: 30,
+                  hasShadow: true,
+                  onTap: () => _becomeAMemberDialog(context),
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 20,
                       ),
-                    )),
-                    DataColumn(
-                        label: Expanded(
-                      child: Text(
-                        "Energy points",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: getAppColorScheme(context).primary),
+                      Icon(
+                        Icons.person_add_alt_1,
+                        color: getAppColorScheme(context).primary,
                       ),
-                    ))
-                  ],
-                  rows: allUsers
-                      .map((e) => DataRow(onSelectChanged: (_) {}, cells: [
-                            DataCell(Text(e["name"])),
-                            DataCell(Text(e["ep"].toString()))
-                          ]))
-                      .toList()),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "Become a member !",
+                        style: getAppTextTheme(context).bodyText1,
+                      ),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 18, bottom: 18),
+                    child: Text(
+                      "Already a member?",
+                      style: getAppTextTheme(context).subtitle1,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                MyPaper(
+                  padding:
+                      const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                  child: DataTable(
+                      showCheckboxColumn: false,
+                      columns: [
+                        DataColumn(
+                            label: Expanded(
+                          child: Text(
+                            "Name",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: getAppColorScheme(context).primary),
+                          ),
+                        )),
+                        DataColumn(
+                            label: Expanded(
+                          child: Text(
+                            "Energy points",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: getAppColorScheme(context).primary),
+                          ),
+                        ))
+                      ],
+                      rows: allUsers
+                          .map((e) => DataRow(onSelectChanged: (_) {}, cells: [
+                                DataCell(Text(e["name"])),
+                                DataCell(Text(e["ep"].toString()))
+                              ]))
+                          .toList()),
+                ),
+              ],
             )));
   }
+}
+
+Future _becomeAMemberDialog(context) {
+  Widget _dialogActionText(onPressed, text) => TextButton(
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: getAppColorScheme(context).primary),
+      ));
+
+  String firstName = "";
+  String lastName = "";
+
+  final _formKey = GlobalKey<FormState>();
+
+  final InputBorder borderDecoration =
+      OutlineInputBorder(borderRadius: BorderRadius.circular(9));
+
+  return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Become a member",
+                          style: getAppTextTheme(context).headline5,
+                        ),
+                        const SizedBox(
+                          height: 29,
+                        ),
+                        Column(
+                          children: [
+                            TextFormField(
+                              autofocus: true,
+                              textCapitalization: TextCapitalization.words,
+                              validator: (value) =>
+                                  value!.isEmpty ? "Write last name" : null,
+                              onChanged: (value) {
+                                setState(() {
+                                  firstName = value;
+                                });
+                              },
+                              decoration: InputDecoration(
+                                labelText: "First name",
+                                labelStyle: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                                hintText: "John",
+                                border: borderDecoration,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 9,
+                            ),
+                            TextFormField(
+                              textCapitalization: TextCapitalization.words,
+                              onChanged: (value) {
+                                setState(() {
+                                  lastName = value;
+                                });
+                              },
+                              validator: (value) =>
+                                  value!.isEmpty ? "Write last name" : null,
+                              decoration: InputDecoration(
+                                labelText: "Last name",
+                                labelStyle: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                                hintText: "Doe",
+                                border: borderDecoration,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 29,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            _dialogActionText(() {
+                              if (_formKey.currentState!.validate()) {
+                                Navigator.pop(context);
+                              }
+                            }, "Cancel"),
+                            _dialogActionText(() {
+                              if (_formKey.currentState!.validate()) {
+                                Navigator.pop(context);
+                              }
+                            }, "Add !"),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      });
 }
