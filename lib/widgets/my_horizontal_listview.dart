0bb16ba1ac061bb7143ed293ad3bad22f945370e
Toke Friis <tokefriis@gmail.com>
2023-06-24 23:48:57 +0200

    moved MyHorizontalListView widget to seperate file

---
+++ lib/widgets/my_horizontal_listview.dart
@@ -0,0 +1,103 @@
+import 'package:energy_of_hco/helpers/app_theme_helper.dart';
+import 'package:flutter/material.dart';
+
+class MyHorizontalListView extends StatelessWidget {
+  const MyHorizontalListView(
+      {Key? key,
+      required this.chosenItems,
+      required this.onChange,
+      required this.allItems,
+      required this.allTitles,
+      this.scaleFactor,
+      this.showCheckMark})
+      : super(key: key);
+  final List<dynamic> chosenItems;
+  final List<dynamic> allItems;
+  final Function(dynamic) onChange;
+  final List<String> allTitles;
+  final double? scaleFactor;
+  final bool? showCheckMark;
+
+  List<Color> _getButtonColors(dynamic currentIndexCategories, context) {
+    // the first item is the background
+    // the second is the textcolor
+    if (chosenItems.contains(currentIndexCategories)) {
+      return [
+        getAppColorScheme(context).primary,
+        getAppColorScheme(context).onPrimary
+      ];
+    }
+    return [
+      getAppColorScheme(context).onPrimary,
+      getAppTextTheme(context).bodyText2!.color!
+    ];
+  }
+
+  Icon getIconFromCheckState(bool isChecked, Color color) {
+    return isChecked
+        ? Icon(
+            Icons.check_circle,
+            color: color,
+          )
+        : Icon(
+            Icons.check_circle_outline,
+            color: color,
+          );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.scale(
+      alignment: Alignment.centerLeft,
+      scale: scaleFactor ?? 1,
+      child: SizedBox(
+        height: 50,
+        child: ListView.separated(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              dynamic currentIndexItem = allItems[index];
+              return GestureDetector(
+                onTap: () => onChange(currentIndexItem),
+                child: Container(
+                  padding: EdgeInsets.all(7),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(17),
+                    color: _getButtonColors(currentIndexItem, context)[0],
+                  ),
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: FittedBox(
+                    fit: BoxFit.fitHeight,
+                    child: Row(
+                      children: [
+                        showCheckMark == true
+                            ? Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 4),
+                                child: getIconFromCheckState(
+                                    chosenItems.contains(currentIndexItem),
+                                    _getButtonColors(
+                                        currentIndexItem, context)[1]),
+                              )
+                            : SizedBox(),
+                        Text(
+                          allTitles[index],
+                          style: TextStyle(
+                              color: _getButtonColors(
+                                  currentIndexItem, context)[1]),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const SizedBox(
+                  width: 8,
+                ),
+            itemCount: allItems.length),
+      ),
+    );
+  }
+}
